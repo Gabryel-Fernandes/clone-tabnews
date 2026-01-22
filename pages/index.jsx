32f031b5f8f0d265de8f75bfd9c7bdffdda31a85
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2026-01-21 22:42:19 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Eu te amo... Muito...</h1>
+function Home() {
+  return <h1>Eu te amo... Muito...</h1>;
+}
+function test() {
+  console.log("test");
+}
+function test2() {
+  console.log("indentação errada");
 }
 
-export default Home
+export default Home;
