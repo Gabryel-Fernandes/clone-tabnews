4c2f801ad0c0764ee4ea3bb49fa076b2c7e9d7c0
Gabryel Fernandes <Gabryelfernandes67@gmail.com>
2026-03-17 17:55:35 -0300

    dev dependeces: adiciona dependencia de desenvolvimento Jest

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,5 @@
 function Home() {
   return <h1>Eu te amo... Muito...</h1>;
 }
-function test() {
-  console.log("test");
-}
-function test2() {
-  console.log("indentação errada");
-}
 
 export default Home;
